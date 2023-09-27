cc6121439fad63d97becd44f47afee98b4d44b08
Leo Janzen <leojanzen13@gmail.com>
2023-09-26 22:27:56 -0700

    Added wireless print

--- main/main.ino
+++ main/main.ino
@@ -9,22 +9,19 @@
 
 //Function prototypes
 void writeLine(unsigned long id, uint8_t len, uint8_t* bytes);
-void canOps(FlexCAN bus);
+void write_message_to_file(CAN_message_t msg);
 void dyingGasp();
 char* nextFileName();
 char* numToChar(int num);
 // Callback function used by the xsens library
 void imu_callback(XsensEventFlag_t event, XsensEventData_t *mtdata);
+void send_wireless_data(CAN_message_t msg);
 
 //Variables and initialization
-const int sdSlot = BUILTIN_SDCARD;
 File file;
 bool isRunning = true;
-CAN_message_t msg;
-unsigned long t;
 unsigned long gps_data;
-
-xsens_interface_t imu_interface = XSENS_INTERFACE_RX( &imu_callback );
+xsens_interface_t imu_interface = XSENS_INTERFACE_RX(&imu_callback);
 
 void setup(){
 	// LED's
@@ -36,45 +33,49 @@ void setup(){
 	pinMode(29, OUTPUT); // LED2
 	
 	// initialize data protocalls
-  Serial.begin(112500);
+  Serial.begin(112500);    // serial port UART
 	Serial1.begin(2000000);  //xsens uart
+  Serial2.begin(9600, SERIAL_8N1);  // ESP32 UART
+
 	//initialize the can buses
-	Can0.begin(125000);
-	//Can1.begin(1000000);
+	Can0.begin(500000);
+	Can1.begin(500000);
 	
+  delay(2000);
+
 	// see if the card is present and can be initialized:
-	SD.begin(sdSlot);
+	SD.begin(BUILTIN_SDCARD);
 	//delay(1000);
 	
 	file = SD.open(nextFileName(), FILE_WRITE); // helper function nextFileName creates a new file so previous data isn't overwritten
 	if(!file)
+  {
 		digitalWrite(28, HIGH);
+  }
 
 	//Dying Gasp Setup - NEED TO ADD BIGGER CAP, OTHERWISE SD CARD WON'T OPEN
 	attachInterrupt(24, dyingGasp, FALLING); //go to dyingGasp function when pin 24 goes from HIGH to LOW  
-	/*unsigned long endSetup = micros();
-	while((micros() - endSetup) < 10000000){
-		if(rf95.recv(datapacket, &len)){
-			if(datapacket[0] == 69)
-				progMode();
-		}
-	}*/
+
+  Serial2.println("Connected!\n    ");
 }
 
 void loop(){
 	// boolean to only read data when we want it to
 	if(isRunning){
+    CAN_message_t msg; 
 		// checks the 0th CAN bus to read data
 		//Serial.println(Can0.available());
     if(Can0.available()){
       Can0.read(msg);
-			canOps();
+			write_message_to_file(msg);
+      send_wireless_data(msg);
 		}
 		// check the 1st CAN bus to read data
-		//if(Can1.available()){
-		//	Can1.read(msg);
-    //  canOps();
-		//}
+		if(Can1.available()){
+			Can1.read(msg);
+      write_message_to_file(msg);
+      send_wireless_data(msg);
+		}
    
 		// Inbound serial data needs to be parsed
     // When a valid packet is decoded, imu_callback() will fire
@@ -85,41 +86,35 @@ void loop(){
 }
 
 // function for recording can data, takes a bus as a parameter and writes that information
-void canOps(){
-	t = micros(); // records the time it reads the message
+void write_message_to_file(CAN_message_t msg){
+  if(!file)  
+  {
+    return;  // exit if SD card is not mounted
+  }
+  
+	unsigned long t = micros(); // records the time it reads the message
 	// print the data in a csv format
   
 	file.print(msg.id, HEX);
 	file.print(",");
-	for(byte i = 0; i < msg.len; i++){
+	for(byte i = 0; i < msg.len; i++)
+  {
 		file.print(msg.buf[i]);
 		file.print(",");
 	}
 	for(byte i = (msg.len); i<8; i++)
+  {
 		file.print(",");
+  }
 	file.println(t);
+  file.flush();  // synchronize file to avoid corruption on power loss
   Serial.println("CAN");
-
-	// if(msg.id == filter){
-	// 	sendDataToLong(msg.id, msg.len, msg.buf);
-	// }
-	//digitalWrite(29, HIGH);*/
-}
-
-//close file on interrupt and stop collecting data
-void dyingGasp(){
-	isRunning = false;
-	file.print("\n");
-	file.print(0x800);
-	file.close();
-  digitalWrite(13, HIGH);
-	while(true){} //the shadow realm
 }
 
 void writeLine(uint32_t id, uint8_t len, float* bytes){
   Serial.println("IMU");
   digitalWrite(29, HIGH);
-  t = micros(); // records the time it reads the message
+  unsigned long t = micros(); // records the time it reads the message
   // print the data in a csv format
   file.print(id, HEX);
   file.print(",");
@@ -130,6 +125,7 @@ void writeLine(uint32_t id, uint8_t len, float* bytes){
   for(byte i = len; i<8; i++)
     file.print(",");
   file.println(t);
+  file.flush();  // synchronize file to avoid corruption on power loss
 }
 
 // Called when the library decoded an inbound packet
@@ -170,10 +166,49 @@ void imu_callback(XsensEventFlag_t event, XsensEventData_t *mtdata)
         case XSENS_EVT_FREE_ACCELERATION:
           if(mtdata->type == XSENS_EVT_TYPE_FLOAT3)
             {
-                writeLine(0xBBB), 3, mtdata->data.f4x3);
+                writeLine(0xBBB, 3, mtdata->data.f4x3);
             }
-          
+            break;
     }
+}
+
+void send_wireless_data(CAN_message_t msg)
+{
+  // arduino IDE gets mad if you declare variables inside the switch
+  static unsigned int last_vcu_state = 0;
+  unsigned int vcu_state = 0;
+  int16_t temp_phaseA = 0;
+  int16_t temp_phaseB = 0;
+  int16_t temp_phaseC = 0;
+  int16_t temp_gate_driver = 0;
+  unsigned int torque = 0;
+
+  switch(msg.id)
+  {
+    case 0x766:  // VCU status
+      
+      vcu_state = msg.buf[4]; 
+      if(last_vcu_state != vcu_state)
+      {
+        Serial2.printf("VCU state transition: %u -> %u\n", last_vcu_state, vcu_state);
+        last_vcu_state = vcu_state;
+      }
+      break;
+    case 0x0C0:  // torque request
+      torque = ((msg.buf[1] << 8) + msg.buf[0])/10;
+      Serial2.printf("Torque request: %uNm\n", torque);
+      break;
+    case 0x380:  // BMS status
+      Serial2.printf("BMS temp: %uC\n", msg.buf[0]);
+      break;
+    case 0x0A0: // MC temps 1
+      temp_phaseA = ((msg.buf[1] << 8) + msg.buf[0])/10;
+      temp_phaseB = ((msg.buf[3] << 8) + msg.buf[2])/10;
+      temp_phaseC = ((msg.buf[5] << 8) + msg.buf[4])/10;
+      temp_gate_driver = ((msg.buf[7] << 8) + msg.buf[6])/10;
+      Serial2.printf("Phase A: %iC, Phase B: %iC, Phase C: %iC, Gate Driver: %iC\n", temp_phaseA, temp_phaseB, temp_phaseC, temp_gate_driver);
+      break;
+  }
 }
 
 // names files 000, 001, etc.
@@ -218,4 +253,14 @@ char* numToChar(int num){
   output[1] = nums[tens];
   output[2] = nums[ones];
   return output;
+}
+
+//close file on interrupt and stop collecting data
+void dyingGasp(){
+	isRunning = false;
+	file.print("\n");
+	file.print(0x800);
+	file.close();
+  digitalWrite(13, HIGH);
+	while(true){} //the shadow realm
 }
